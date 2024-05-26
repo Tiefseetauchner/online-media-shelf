b2a884b48449e084baa60096408800fbd0c6e796
tiefseetauchner <lena@tauchner.me>
2024-05-26 14:32:24 +0200

    #7 - make barcode unique

---
+++ OnlineMediaShelf.Domain/Migrations/20240526123200_MakeBarcodeUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tiefseetauchner.OnlineMediaShelf.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeBarcodeUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_Barcode",
+                table: "Items",
+                column: "Barcode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Items_Barcode",
+                table: "Items");
+        }
+    }
+}
