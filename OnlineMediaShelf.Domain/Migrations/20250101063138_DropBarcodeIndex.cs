33ae7819e868eb0dd32d3c1ed342abd7ce3751f9
Lena Tauchner <lena@tauchner.me>
2025-01-01 07:37:41 +0100

    - migration

---
+++ OnlineMediaShelf.Domain/Migrations/20250101063138_DropBarcodeIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tiefseetauchner.OnlineMediaShelf.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropBarcodeIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ItemData_Barcode_Version",
+                table: "ItemData");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ItemData_Barcode_Version",
+                table: "ItemData",
+                columns: new[] { "Barcode", "Version" },
+                unique: true);
+        }
+    }
+}
